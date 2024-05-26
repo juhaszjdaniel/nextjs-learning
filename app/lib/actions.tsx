c5e5668826d118b2283bf5fd4475bfba2ea54080
juhaszjdaniel <juhasz.j.daniel@gmail.com>
2024-05-26 14:57:53 +0200

    add authentication

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -3,6 +3,8 @@ import { z } from 'zod';
 import {sql} from "@vercel/postgres";
 import {revalidatePath} from "next/cache";
 import {redirect} from "next/navigation";
+import {AuthError} from "next-auth";
+import {signIn} from "@/auth";
 
 const FormSchema = z.object({
     id: z.string(),
@@ -101,5 +103,24 @@ export async function deleteInvoice(id: string) {
         revalidatePath('/dashboard/invoices');
     } catch (error) {
         return { message: 'An error occurred while deleting the invoice' };
+    }
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
     }
 }
